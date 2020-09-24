a95e8342a697cf5e40c62c48490b4ebd27254d36
luckyandrean <71387629+luckyandrean@users.noreply.github.com>
2020-09-24 09:21:27 +0700

    Add files via upload

---
+++ build/precache-manifest.eca792442903ba2d5a83107bdaf583cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "534931a256acea9ef99cabec13777908",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "80b307c8b666b0a81579",
+    "url": "/movieapp/static/css/main.eb70be42.chunk.css"
+  },
+  {
+    "revision": "3921fc2f910cccf35505",
+    "url": "/movieapp/static/js/2.e5d701a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movieapp/static/js/2.e5d701a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80b307c8b666b0a81579",
+    "url": "/movieapp/static/js/main.df0d90c6.chunk.js"
+  },
+  {
+    "revision": "4788b2c78e3dc21468ec",
+    "url": "/movieapp/static/js/runtime-main.857852d7.js"
+  }
+]);
